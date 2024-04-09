464d084ba7c384534575dbc5cafeb06304e425f5
Md Tuhin Mia <mtuhinahmed95@gmail.com>
2024-04-09 21:15:39 +0600

    update

---
+++ final-evidance-php/project.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Course selling</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    
+    <div class="container-fluid"> 
+    <h3><a href="#">Studylab</a></h3>
+        <div class="navbar"> 
+        
+        <a href="#">Home</a>
+        <a href="#">About</a>
+        <a href="#">Course</a>
+        <a href="#">Instructor</a>
+        <a href="#">Blog</a>
+        <a href="#">Contact</a>
+
+        </div>
+
+
+        <div class="main-body"> 
+            <form action="#"> 
+                <label for="name">Full Name</label><br>
+                <input type="text" name="name"><br><br>
+
+                <label for="email">Email Address</label><br>
+                <input type="email" name="email" placeholder="enter your email"><br><br>
+
+                <label for="password">Password</label><br>
+                <input type="password" name="password" placeholder="enter your password"><br><br>
+
+                <label for="cpassword">Confirm Password</label><br>
+                <input type="password" name="cpassword" placeholder="enter your confirm password"><br><br>
+
+                <input type="submit" name="submit" value="submit">
+            </form>
+        </div>
+
+        <h1>We Are Online Platfrom</h1>
+        <h1>For Make Learn</h1>
+
+
+
+    </div>
+
+
+    
+</body>
+</html>
