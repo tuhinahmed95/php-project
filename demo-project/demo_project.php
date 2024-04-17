c48ea585b307c2fba0fa7353ce02e464840e847c
Md Tuhin Mia <mtuhinahmed95@gmail.com>
2024-04-17 10:18:55 +0600

    update

--- demo-project/demo_project.php
+++ demo-project/demo_project.php
@@ -17,7 +17,21 @@
     <form action="#"> 
         <label for="id"> ID
             <input type="text" name="id">
+        </label><br><br> 
+
+        <label for="name">NAME 
+            <input type="text" name="name">
         </label>
+
+        <label for="gpa">GPA
+            <input type="text" name="gpa">
+        </label><br><br>
+
+        <label for="roll">ROLL 
+            <input type="text" name="roll">
+        </label><br><br>
+
+        <input type="submit" name="btn" value="submit">
     </form>
     
 </body>
