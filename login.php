2fffb5d3c843253d217a7b4ec074161552373314
Md Tuhin Mia <mtuhinahmed95@gmail.com>
2024-04-24 00:49:03 +0600

    login

---
+++ login.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <center>
+        <fieldset>
+            <legend>Login Form</legend>
+    <div class="container" > 
+    
+
+         
+            <form action="#" method="POST"> 
+                Username <br>
+                <input type="text" name="user" required=""><br><br>
+                Password <br>
+                <input type="text" name="pass" required=""><br><br>
+                <input type="submit" name="btn" value="Login">
+            </form>
+        
+    </div>
+    </fieldset>
+    </center>
+    
+    
+</body>
+</html>
