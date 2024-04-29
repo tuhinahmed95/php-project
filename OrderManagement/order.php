71d924572946cb8cab6f6e924482fdf6c2966a2f
Hassan Mahfuj <humahfuj@gmail.com>
2024-04-29 16:03:52 +0600

    order

---
+++ OrderManagement/order.php
@@ -0,0 +1,54 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ordermanagement</title>
+    <link rel="stylesheet" href="style.css">
+
+</head>
+<body>
+
+        <div class="container"> 
+
+            <div class="header"> 
+                <h1 class="first">Hi,Welcome Back</h1>                
+                <h3 class="five">All Sales (Offline) <br> 932,210</h3>
+                <h3 class="six">All Sales (Online) <br> 431,007</h3>
+                <h3 class="seven">Customer Rating <br> 12,775</h3>
+                
+            </div>
+
+            
+
+            <div class="main"> 
+                <h3 class="first-div">Start Date <br>April 01/2024</h3>
+                <h3 class="second-div">Start Date <br> April 30/2024</h3>
+                <h3 class="third-div">Sales Mesure <br>Revenue</h3>
+                <h3 class="four-div">Customer Type <br>All Customer</h3>
+                <h3 class="five-div">Transaction Type <br> All Transaction</h3>
+                
+            </div>
+
+
+            <div class="main-body"> 
+                <h4 class="main-div-first">Total Quantity <br> <br> 110,000 <br>2.00%(30 days)</h4>
+                <h4 class="main-div-second">Total Cost <br> <br> $523,200 <br> 12.09%(30 Days)</h4>
+                <h4 class="main-div-third">Total Revenue <br><br>$753,098 <br>0.51%(30 Days)</h4>
+                <h4 class="main-div-four">Total Profit <br> <br> $331,886 <br> 5.32%(30 Days)</h4>
+            </div>
+
+            <div class="footer"> 
+                <h2 class="footer-div">THIS YEAR'S TOTAL REVENUE <br> Sales Performance For Online And Ofline Revenue</h2>
+                <h2 class="footer-right-div">SALES REVENUE CUSTOMERS IN USA <br> Sales Performance Of All States In The United States</h2>
+            </div>
+
+            
+
+            
+
+        </div>
+    
+</body>
+</html>
