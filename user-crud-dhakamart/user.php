aab388270608c9fbaf6b77dd9fef94a168b62420
Hassan Mahfuj <humahfuj@gmail.com>
2024-05-05 15:00:09 +0600

    user

---
+++ user-crud-dhakamart/user.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>User</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+    <?php
+
+    $connect = mysqli_connect ("localhost","root","","dhakamart");
+
+
+    ?>
+
+    <form action="#" method="POST">  
+        <input type="id">
+    </form>
+    
+</body>
+</html>
